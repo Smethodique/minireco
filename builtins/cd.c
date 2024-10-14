a56db7b17c11196f948048bb4da3fd6299665f66
Smethodique <takhtoukhsami@gmail.com>
2024-10-14 17:50:01 +0100

    hi

--- builtins/cd.c
+++ builtins/cd.c
@@ -1,10 +1,11 @@
 #include "../minishell.h"
 
+
 void	update_old_pwd(char *old_pwd, char **env)
 {
 	int i;
 
-	i = 0;		
+	i = 0;
 	while (env[i])
 	{
 		if (ft_strncmp(env[i], "OLDPWD=", 7) == 0)
@@ -18,7 +19,7 @@ void	update_pwd(char *pwd, char **env)
 	int i = 0;
 	while (env[i])
 	{
-		if(ft_strncmp(env[i], "PWD=", 4) == 0)
+		if (ft_strncmp(env[i], "PWD=", 4) == 0)
 			env[i] = ft_strjoin("PWD=", pwd);
 		i++;
 	}
@@ -26,8 +27,8 @@ void	update_pwd(char *pwd, char **env)
 
 char	*ft_chr(char **env, char *vrb)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 
 	i = 0;
 	while (env[i])
@@ -39,7 +40,6 @@ char	*ft_chr(char **env, char *vrb)
 				j++;
 			if (env[i][j] == '=' && !vrb[j])
 			{
-				free(vrb);
 				return (ft_strdup(*(env + i) + j + 1));
 			}
 		}
@@ -49,26 +49,18 @@ char	*ft_chr(char **env, char *vrb)
 	return (NULL);
 }
 
-
-void    cd(t_command *cmd, char **env)
+void	handle_cd(t_command *cmd, char **env)
 {
-	char *old_p;
-	char *cwd;
 	char *home;
-	old_p = getcwd(NULL, 1000);
-	update_old_pwd(old_p, env);
-	if (cmd->args[2])
-	{
-		ft_putstr_fd("minishell: too many arguments\n", 2);
-		return;
-	}
 
-	else if(!ft_strncmp(cmd->args[1], "-", 2))
+	if (!ft_strncmp(cmd->args[1], "-", 2))
 	{
 		chdir(ft_chr(env, "OLDPWD"));
 		pwd(cmd, env);
+		if (!ft_chr(env, "OLDPWD"))
+			ft_putstr_fd("minishell: OLDPWD not set\n", 2);
 	}
-	else if (cmd->args[1] == NULL || !ft_strncmp(cmd->args[1] ,"~", 2))
+	else if (cmd->args[1] == NULL || !ft_strncmp(cmd->args[1], "~", 2))
 	{
 		if (ft_chr(env, "HOME"))
 		{
@@ -84,6 +76,22 @@ void    cd(t_command *cmd, char **env)
 	{
 		perror("minishell");
 	}
+}
+
+void	cd(t_command *cmd, char **env)
+{
+	char *old_p;
+	char *cwd;
+
+	old_p = getcwd(NULL, 1000);
+	update_old_pwd(old_p, env);
+	if (cmd->args[2])
+	{
+		ft_putstr_fd("minishell: too many arguments\n", 2);
+		return ;
+	}
+
+	handle_cd(cmd, env);
 	cwd = getcwd(NULL, 1000);
 	update_pwd(cwd, env);
 }
