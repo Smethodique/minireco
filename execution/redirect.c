a875243a45b0e38b78a03d75b097c44c4d238701
samiT <86780420+Smethodique@users.noreply.github.com>
2024-10-18 15:54:29 +0100

    Update redirect.c

--- execution/redirect.c
+++ execution/redirect.c
@@ -1,86 +1,93 @@
 #include "../minishell.h"
 
-void exec_in_child(t_command *cmd, char **env)
+
+void    exec_in_child(t_command *cmd, char **env)
 {
-	cmd->args[0] = get_path(cmd->args, env); 
-	if (execve(cmd->args[0], cmd->args, env) == -1)
-	{
-		perror("minishell wst el exec d pip");
-		exit(127);
-	}
+        cmd->args[0] = get_path(cmd->args, env);
+        if (execve(cmd->args[0], cmd->args, env) == -1)
+        {
+                perror("minishell: execution failed");
+                exit(127);
+        }
 }
 
-void redic_builtin(t_command *cmd, char **env)
+void    redic_builtin(t_command *cmd, char **env)
 {
-	int in = 0;
-	int out = 1;
-	int in_fd = dup(STDIN_FILENO);
-	int out_fd = dup(STDOUT_FILENO);
-	in = get_in(cmd, in);
-	out = get_out(cmd, out);
-	if (in == -1 || out == -1)
-	{
-		perror("minishell");
-		exit(1);
-	}
-	dup_in_out(in, out);
-	execute_builtin(cmd, env, is_builtin(cmd));
-	restore_fd(in, out, in_fd, out_fd);
-	close(in);
-	close(out);
+        int in = dup(STDIN_FILENO);
+        int out = dup(STDOUT_FILENO);
+        int new_in = get_in(cmd, STDIN_FILENO);
+        int new_out = get_out(cmd, STDOUT_FILENO);
+
+        if (new_in == -1 || new_out == -1)
+                return ;
+        dup2(new_in, STDIN_FILENO);
+        dup2(new_out, STDOUT_FILENO);
+        execute_builtin(cmd, env, is_builtin(cmd));
+        dup2(in, STDIN_FILENO);
+        dup2(out, STDOUT_FILENO);
+        close(in);
+        close(out);
+        if (new_in != STDIN_FILENO)
+                close(new_in);
+        if (new_out != STDOUT_FILENO)
+                close(new_out);
 }
 
-void redic_not_builtin(t_command *cmd, char **env)
+void    setup_redirection(t_command *cmd)
 {
-    int in = 0;
-    int out = 1;
-    int pid = fork();
-    
-    if (pid == 0)
-    {
-        signal(SIGINT, SIG_DFL);
-        signal(SIGQUIT, SIG_DFL);
-        
-        // Get input/output redirections
-        in = get_in(cmd, in);
-        out = get_out(cmd, out);
-        
+        int in = get_in(cmd, STDIN_FILENO);
+        int out = get_out(cmd, STDOUT_FILENO);
+
         if (in == -1 || out == -1)
         {
-            perror("minishell");
-            exit(1);
+                perror("minishell");
+                exit(1);
         }
-        
-        // Set up redirections before executing
-        if (in != 0)
+
+        if (in != STDIN_FILENO)
         {
-            dup2(in, STDIN_FILENO);
-            close(in);
+                dup2(in, STDIN_FILENO);
+                close(in);
         }
-        if (out != 1)
+        if (out != STDOUT_FILENO)
         {
-            dup2(out, STDOUT_FILENO);
-            close(out);
+                dup2(out, STDOUT_FILENO);
+                close(out);
         }
-        
-        exec_in_child(cmd, env);
-        exit(1);
-    }
-    else 
-    {
-        int status;
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status))
-             g_vars.exit_status = WEXITSTATUS(status);
-        else
-             g_vars.exit_status = 128 + WTERMSIG(status);
-    }
 }
 
-void ft_redict(t_command *cmd, char **env)
+void    redic_not_builtin(t_command *cmd, char **env)
 {
-	if (is_builtin(cmd) == NOT_BUILT_IN)
-		redic_not_builtin(cmd, env);
-	else
-		redic_builtin(cmd, env);
+        pid_t pid = fork();
+
+        if (pid == 0)
+        {
+                signal(SIGINT, SIG_DFL);
+                signal(SIGQUIT, SIG_DFL);
+
+                setup_redirection(cmd);
+
+                exec_in_child(cmd, env);
+        }
+        else if (pid > 0)
+        {
+                int status;
+                waitpid(pid, &status, 0);
+                if (WIFEXITED(status))
+                        g_vars.exit_status = WEXITSTATUS(status);
+                else if (WIFSIGNALED(status))
+                        g_vars.exit_status = 128 + WTERMSIG(status);
+        }
+        else
+        {
+                perror("minishell: fork failed");
+        }
+}
+
+void    ft_redict(t_command *cmd, char **env)
+{
+        if (is_builtin(cmd) == NOT_BUILT_IN)
+                redic_not_builtin(cmd, env);
+        else
+                redic_builtin(cmd, env);
 }
